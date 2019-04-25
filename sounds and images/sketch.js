b4f90d44b531627990d504b62ac7019956d07d32
mhsNBalkovec <48356549+mhsNBalkovec@users.noreply.github.com>
2019-04-25 09:48:32 -0400

    Update sketch.js

--- sounds and images/sketch.js
+++ sounds and images/sketch.js
@@ -3,13 +3,16 @@ var meme2;
 
 function preload() {
   // Loads the sound file into the variable
+  soundFormats('mp3', 'ogg');
   meme = loadSound('rickrollcut.mp3');
   meme2 = loadImage('rick.png');
 }
 
 function setup() {
   createCanvas(1021, 768);
+  //plays sound
   meme.setVolume(0.15);
   meme.play();
+  //loads image onto canvas
   image(meme2,0,0);
 }
